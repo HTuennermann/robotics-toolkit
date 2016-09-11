455e9f4194ec0f33cce85f87102cfeacc3a6f83f
川岸 照代 <Teruyo@kawagishi-no-MacBook-Pro.local>
2016-09-11 10:00:36 +0900

    Updated particle filter to use numpy, no good idea as to how to auto test yet

--- particle.py
+++ particle.py
@@ -1,155 +1,82 @@
 import numpy as np
 from numpy import pi
 import random
-
-class Robot:
-    def __init__(self):
-        self.x = np.random.random() * world_size
-        self.y = np.random.random() * world_size
-        self.orientation = np.random.random() * 2.0 * pi
-        self.forward_noise = 0.0
-        self.turn_noise = 0
-        self.sense_noise = 0
-
-    def set(self, new_x, new_y, new_orientation):
-        if new_x < 0 or new_x >= world_size:
-            raise ValueError('X coordinate out of bound')
-        if new_y < 0 or new_y >= world_size:
-            raise ValueError('Y coordinate out of bound')
-        if new_orientation < 0 or new_orientation >= 2 * pi:
-            raise ValueError('Orientation must be in [0..2pi]')
-        self.x = float(new_x)
-        self.y = float(new_y)
-        self.orientation = float(new_orientation)
-
-    def set_noise(self, new_f_noise, new_t_noise, new_s_noise):
-        # makes it possible to change the noise parameters
-        # this is often useful in particle filters
-        self.forward_noise = float(new_f_noise)
-        self.turn_noise = float(new_t_noise)
-        self.sense_noise = float(new_s_noise)
-
-    def sense(self):
-        z = []
-        for i in range(len(landmarks)):
-            dist = np.sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
-            dist += random.gauss(0.0, self.sense_noise)
-            z.append(dist)
-        return z
-
-    def move(self, turn, forward):
-        if forward < 0:
-            raise ValueError('Robot cant move backwards')
-
-            # turn, and add randomness to the turning command
-        orientation = self.orientation + float(turn) + random.gauss(0.0, self.turn_noise)
-        orientation %= 2 * pi
-
-        # move, and add randomness to the motion command
-        dist = float(forward) + random.gauss(0.0, self.forward_noise)
-        x = self.x + (np.cos(orientation) * dist)
-        y = self.y + (np.sin(orientation) * dist)
-        x %= world_size  # cyclic truncate
-        y %= world_size
-
-        # set particle
-        res = Robot()
-        res.set(x, y, orientation)
-        res.set_noise(self.forward_noise, self.turn_noise, self.sense_noise)
-        return res
-
-    def Gaussian(self, mu, sigma, x):
-
-        # calculates the probability of x for 1-dim Gaussian with mean mu and var. sigma
-        return np.exp(- ((mu - x) ** 2) / (sigma ** 2) / 2.0) / np.sqrt(2.0 * pi * (sigma ** 2))
-
-    def measurement_prob(self, measurement):
-
-        # calculates how likely a measurement should be
-
-        prob = 1.0
-        for i in range(len(landmarks)):
-            dist = np.sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
-            prob *= self.Gaussian(dist, self.sense_noise, measurement[i])
-        return prob
-
-    def __repr__(self):
-        return '[x=%.6s y=%.6s orient=%.6s]' % (str(self.x), str(self.y), str(self.orientation))
+from particle_testbot import *
 
 
-def eval(r, p):
-    s = 0.0
-    for i in range(len(p)):  # calculate mean error
-        dx = (p[i].x - r.x + (world_size / 2.0)) % world_size - (world_size / 2.0)
-        dy = (p[i].y - r.y + (world_size / 2.0)) % world_size - (world_size / 2.0)
-        err = np.sqrt(dx * dx + dy * dy)
-        s += err
-    return s / float(len(p))
+def move_particles(particles, turn, turn_noise, forward, forward_noise):
+    dist = forward + np.random.normal(0.0, forward_noise, len(particles[0]))
+    particles[0] = (particles[0] + turn + np.random.normal(0.0, turn_noise, len(particles[0]))) % (2 * pi)
+    particles[1] = (particles[1] + (np.cos(particles[0]) * dist)) % world_size
+    particles[2] = (particles[2] + (np.sin(particles[0]) * dist)) % world_size
 
 
+def measurement_prob(particles, measurement, m_noise):
 
+    prob = np.ones(len(particles[1]))
+
+    for i in range(len(landmarks)):
+        dist = np.sqrt((particles[1] - landmarks[i][0]) ** 2 + (particles[2] - landmarks[i][1]) ** 2)
+        prob *= np.exp(- ((dist - measurement[i]) ** 2) / (m_noise ** 2) / 2.0) / np.sqrt(2.0 * pi * m_noise ** 2)
+
+    return prob
 
 class ParticleFilter:
-    def __init__(self, f_noise, t_noise, s_noise, n):
+    def __init__(self, grid, noise, n):
+        self.noise = noise
         self.N = n
-        self.p = []
-        self.w = []
-        for i in range(N):
-            r = Robot()
-            r.set_noise(f_noise, t_noise, s_noise)
-            self.p.append(r)
+        self.w = np.zeros(self.N)
+        self.particles = np.array([np.random.random(N)*g for g in grid])
 
     def move_particles(self, turn, forward):
-        p2 = []
-        for i in range(self.N):
-            p2.append(self.p[i].move(0.1, 5.0))
-        self.p = p2
+        move_particles(self.particles, turn, self.noise[1], forward, self.noise[0])
 
-    def get_probability(self, Z):
-        self.w = []
-        for i in range(self.N):
-            self.w.append(self.p[i].measurement_prob(Z))
+    def calculate_probabilites(self, sensor_data):
+        self.w = measurement_prob(self.particles, sensor_data, self.noise[2])
 
-    def resample_particle(self):
-        p3 = []
+    def resample_particles(self):
+
+        mw = np.max(self.w)
         index = int(random.random() * N)
         beta = 0.0
-        mw = max(self.w)
+        particles_temp = self.particles.copy()
+
         for i in range(self.N):
             beta += random.random() * 2.0 * mw
             while beta > self.w[index]:
                 beta -= self.w[index]
                 index = (index + 1) % N
-            p3.append(self.p[index])
-        self.p = p3
+            self.particles[:, i] = particles_temp[:, index]
 
 
+def eval(r, p):
+    dx = (p[1] - r.x + (world_size / 2.0)) % world_size - (world_size / 2.0)
+    dy = (p[2] - r.y + (world_size / 2.0)) % world_size - (world_size / 2.0)
+    err = np.sqrt(dx * dx + dy * dy)
+    return sum(err)/len(err)
 
-
-landmarks  = [[20.0, 20.0], [80.0, 80.0], [20.0, 80.0], [80.0, 20.0]]
+landmarks = [[20.0, 20.0], [80.0, 80.0], [20.0, 80.0], [80.0, 20.0]]
 world_size = 100.0
-myrobot = Robot()
-myrobot = myrobot.move(0.1, 5.0)
-Z = myrobot.sense()
-N = 1000
-T = 10
+myrobot = Robot(world_size, landmarks)
 
-f_noise, t_noise, s_noise = 0.05, 0.05, 5.0
+N = 1000
+T = 100
+
+f_noise, t_noise, s_noise = 0.05, 0.05, 5
 turn, forward = 0.1, 5.0
 
+filter = ParticleFilter([2*np.pi, world_size, world_size], [f_noise, t_noise, s_noise], N)
 
-filter = ParticleFilter(f_noise, t_noise, s_noise, N)
 for t in range(T):
+
     myrobot = myrobot.move(turn, forward)
-    Z = myrobot.sense()
+    sensor_data = myrobot.sense()
+
     filter.move_particles(turn, forward)
-    filter.get_probability(Z)
-    filter.resample_particle()
+    filter.calculate_probabilites(sensor_data)
+    filter.resample_particles()
+    print eval(myrobot, filter.particles)
 
 
-
-
-
-    print eval(myrobot, filter.p)
 
 
